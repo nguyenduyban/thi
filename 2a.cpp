fd12651a968ba71390c6ba51de56607433cba56f
nguyenduyban <nguyenduyban02@gmail.com>
2024-11-16 08:49:18 +0700

    local_complete

--- 2a.cpp
+++ 2a.cpp
@@ -1,6 +1,12 @@
 include<iostream>
 using namespace std;
 void main{
+	int n;
 	cout<<"nhap n so nguyen";
 	cin>>n;
+	for(int i=0;i<n;i++){
+		if(i%2==0){
+			cout <<"la so chan";
+		}else cout <<"la so le";
+	}
 }
